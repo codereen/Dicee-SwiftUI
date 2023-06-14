7f2644800817b760bed8e732505184f9be3f1190
Zoya Afreen <zoyafreen04@gmail.com>
2023-06-15 00:56:31 +0530

    Initial Commit

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by Zoya Afreen on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
